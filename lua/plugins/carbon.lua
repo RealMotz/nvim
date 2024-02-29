fce19187fc233ff774b428ea54a6deb980a7a0bc
Carlos Motta <carlos.motta.business@gmail.com>
2024-02-29 23:11:11 +0900

    Add carbon plugin

---
+++ lua/plugins/carbon.lua
@@ -0,0 +1,15 @@
+return {
+  "ellisonleao/carbon-now.nvim",
+  lazy = true,
+  cmd = "CarbonNow",
+  opts = {
+    options = {
+      bg = "rgba(148,226,213,100)",
+      font_family = "Hack",
+      font_size = "18px",
+      theme = "dracula-pro",
+      titlebar = "motz",
+      watermark = false
+    }
+  }
+}
