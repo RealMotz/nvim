2f560ddaa46a0e2db6be08142706fc497e56c173
motz <carlos.motta.business@gmail.com>
2025-02-13 20:10:53 +0900

    fix: remove unused settings. fix dependency errors.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "tsserver", "volar", "rust_analyzer"  },
+				ensure_installed = { "lua_ls", "gopls", "rust_analyzer"  },
 			})
 		end,
 	},
@@ -19,29 +19,8 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.eslint.setup({
-				settings = { packageManager = "npm" },
-				on_attach = function(client, bufnr)
-				  vim.api.nvim_create_autocmd("BufWritePre", {
-				    buffer = bufnr,
-				    command = "EslintFixAll",
-				  })
-				end,
-			})
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-				init_options = { preferences = { disableSuggestions = true } },
-			})
 			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.volar.setup({
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-				init_options = {
-					typescript = {
-						serverPath = "/home/motz/.nvm/versions/node/v21.7.1/lib/node_modules/typescript/lib/tsserverlib.js",
-					},
-				},
-			})
       lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
