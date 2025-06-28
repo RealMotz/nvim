5ebcfbbf55ff1bb23675e4da10be1a5a6559885f
Motta <carlos.motta.business@gmail.com>
2025-06-29 01:39:03 +0900

    fix: conform linting keymap now works

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,19 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua"},
-        python = {"black", "isort"},
-        javascript = {"prettier"},
-        rust = {"rustfmt"},
-        go = {"gofmt", "goimports"},
-        ruby = {"rubocop"},
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				javascript = { "prettier" },
+				rust = { "rustfmt" },
+				go = { "gofmt", "goimports" },
+				ruby = { "rubocop" },
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", function()
+			require("conform").format({ async = true })
+		end, {})
+	end,
 }
